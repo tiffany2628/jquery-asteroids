967a514250d2c40a8c5886b7063d699d79db6824
tiffany2628 <tiffany.sandoval28@gmail.com>
2016-11-03 10:26:52 -0600

    Added h2 tag and notes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
-var url="https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY"
+var url="https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=Qoqj3eYvB0ORuGakXNkAG75owZsuT4l8LP4KPMHN"
 
-$a.jax({
+$.ajax({
   url: url,
   success: function(results) {
-    alert("Got back" + results.element_count + "asteroids");
+    $("#count").html(results.element_count);
+  // Below does the exact same thig, but more longer / unginly
+  // document.getElementById("count").innerHTML = results.element_count;
+
+  var asteroids = results.near_earth_objects["2015-09-08"];
   }
 })
